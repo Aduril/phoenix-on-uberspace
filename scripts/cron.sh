e9b3f154624fc67a5edf054e8e3dadf46f8d0f86
Peter Melinat <petermelint@MBP-von-Peter.fritz.box>
2022-08-07 21:14:09 +0200

    more text

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -8,10 +8,6 @@ if [ "$NO_OF_NEW_COMMITS" -gt "0" ]; then
   source "$HOME/.uberphoenix_env"
   supervisorctl stop uberphoenix
   supervisorctl remove uberphoenix
-  IFS=
-  for l in $(ps -e -o pid,cmd | grep "beam" | grep -v "grep"); do
-    echo $l | cut -f1 -d' ' | xargs kill -9
-  done
   git pull
   mix deps.get
   mix compile
